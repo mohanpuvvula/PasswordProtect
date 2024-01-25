c3e64d97c173980aac6e343d4ffd11430316dfcd
Mohan <mohan.puvvula@outlook.com>
2024-01-25 11:11:09 +0000

    Updated sql_connection.py for database initialization

--- sql_connection.py
+++ sql_connection.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from models import User
+from flask_login import UserMixin
 
 app = Flask(__name__)
 
-# Configure Flask to use PostgreSQL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vzbvabbwraunrq:f5eb32113a778a27aa5eeda81e4fb2876d79e96d13ca67b001aa1cdfb68419e8@ec2-107-21-67-46.compute-1.amazonaws.com:5432/d4hj3ld5o7fdll'
 
-# Initialize the Flask-SQLAlchemy extension
+
 db = SQLAlchemy(app)
 
-# Define your models and other database-related code here
+def create_tables():
 
-if __name__ == '__main__':
-    # Create database tables if they don't exist
     with app.app_context():
         db.create_all()
+
+if __name__ == '__main__':
+    create_tables()
