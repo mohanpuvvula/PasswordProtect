67e0fe50070863837472201ff39f173309777a8c
Mohan <mohan.puvvula@outlook.com>
2024-01-25 11:23:20 +0000

    Updated app.py for database initialization

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_login import LoginManager, login_required
 from models import db, User
 from user import user_bp
 from encryption import encryption_bp
-from flask_login import login_required, current_user, logout_user, login_user
+from flask_login import current_user, logout_user
 import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/myconnection'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'xyzabc'
-login_manager = LoginManager(app)
-login_manager.login_view = 'user.login'
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vzbvabbwraunrq:f5eb32113a778a27aa5eeda81e4fb2876d79e96d13ca67b001aa1cdfb68419e8@ec2-107-21-67-46.compute-1.amazonaws.com:5432/d4hj3ld5o7fdll'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = 'xyzabc'
 
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'user.login'
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
-db.init_app(app)
+    db.init_app(app)
 
-app.register_blueprint(user_bp, url_prefix="/user")
-app.register_blueprint(encryption_bp, url_prefix='/encryption')
+    app.register_blueprint(user_bp, url_prefix="/user")
+    app.register_blueprint(encryption_bp, url_prefix='/encryption')
 
+    return app
+
+app = create_app()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
