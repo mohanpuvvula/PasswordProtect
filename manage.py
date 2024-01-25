eaf8d221d8888cb96dcf8f9495fd43523c51a764
Mohan <mohan.puvvula@outlook.com>
2024-01-25 11:39:51 +0000

    Updated manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask.cli import FlaskGroup
 from app import create_app, db
 
 app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
-
-@app.cli.command("init-db")
+# Create a command to initialize the database
+@cli.command("init-db")
 def init_db():
     with app.app_context():
         db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    cli()
