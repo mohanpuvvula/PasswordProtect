e29b10963c521ea114eee6c20b6e75e971c79dee
Mohan <mohan.puvvula@outlook.com>
2024-01-26 12:11:22 +0000

    database changes

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_login import LoginManager, login_required
 from models import db, User
 from user import user_bp
 from encryption import encryption_bp
-from flask_login import current_user, logout_user
+from flask_login import login_required, current_user, logout_user, login_user
 import os
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vzbvabbwraunrq:f5eb32113a778a27aa5eeda81e4fb2876d79e96d13ca67b001aa1cdfb68419e8@ec2-107-21-67-46.compute-1.amazonaws.com:5432/d4hj3ld5o7fdll'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.secret_key = 'xyzabc'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/myconnection'
 
-    login_manager = LoginManager(app)
-    login_manager.login_view = 'user.login'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'xyzabc'
+login_manager = LoginManager(app)
+login_manager.login_view = 'user.login'
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
-    db.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    app.register_blueprint(user_bp, url_prefix="/user")
-    app.register_blueprint(encryption_bp, url_prefix='/encryption')
+db.init_app(app)
 
-    return app
+app.register_blueprint(user_bp, url_prefix="/user")
+app.register_blueprint(encryption_bp, url_prefix='/encryption')
 
-app = create_app()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -60,6 +57,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
--- sql_connection.py
+++ sql_connection.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vzbvabbwraunrq:f5eb32113a778a27aa5eeda81e4fb2876d79e96d13ca67b001aa1cdfb68419e8@ec2-107-21-67-46.compute-1.amazonaws.com:5432/d4hj3ld5o7fdll'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/myconnection'
 
 
 db = SQLAlchemy(app)
